bb92c6b75164b757509d42a0dab39d3cb3fc1ce6
Felix Albrecht <mail@felixalbrecht.de>
2013-06-28 00:05:35 +0200

    updated license information to be less than 80 chars wide

--- dune/pymor/exists.hh
+++ dune/pymor/exists.hh
@@ -1,4 +1,5 @@
-// This file is part of the dune-pymor project (https://github.com/pyMor/dune-pymor).
+// This file is part of the dune-pymor project:
+//   https://github.com/pyMor/dune-pymor
 // Copyright Holders: Felix Albrecht, Stephan Rave
 // License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
