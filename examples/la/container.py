195fb18c5ebd3232e0f87258ee4618897d9ac488
Felix Albrecht <mail@felixalbrecht.de>
2013-07-01 18:58:44 +0200

    [examples.la.container] added python bindings

---
+++ examples/la/container.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+import sys, os
+import pybindgen
+from dune.pymor.core import create_module as create_dune_module
+from dune.pymor.la.container import add_VectorInterface as add_dune_Vector
+
+
+if __name__ == '__main__':
+    # prepare
+    assert(len(sys.argv) > 1)
+    inputdir, outputdir, includedirs = sys.argv[1], sys.argv[2], sys.argv[3:]
+    includedirs = includedirs[0].split(';')
+    generator_filename = os.path.join(outputdir, 'dunepymor_container_example.cc')
+    # add dune-pymor constructs
+    module = create_dune_module('lacontainerexample', 'container.hh')
+    module = add_dune_Vector(module, 'Dune::Pymor::LA::EigenDenseVector')
+    # add user code stuff
+    module.add_function('createVector',
+                        pybindgen.retval('Dune::Pymor::LA::EigenDenseVector *', caller_owns_return=True),
+                        [pybindgen.param('int', 'ss')])
+    with open(generator_filename, 'wb') as output:
+        module.generate(pybindgen.FileCodeSink(output))
