9f1e35cc1d7aba8bf9b2a079764acf946dac1ac9
Felix Albrecht <mail@felixalbrecht.de>
2013-07-01 18:56:45 +0200

    [la.container.interfaces] really default to double

--- dune/pymor/la/container/interfaces.hh
+++ dune/pymor/la/container/interfaces.hh
@@ -43,45 +43,45 @@ public:
    * \return          Truth value of the comparison.
    */
   virtual bool almost_equal(const VectorInterface* other,
-                            const ScalarType epsilon = Dune::DefaultEpsilon< ScalarType >::value()) const = 0;
+                            const double epsilon = Dune::FloatCmp::DefaultEpsilon< double >::value()) const = 0;
 
   /**
    * \brief BLAS SCAL operation (in-place sclar multiplication).
    * \param alpha The scalar coefficient with which the vector is multiplied.
    */
-  virtual void scal(const ScalarType alpha) = delete;
+  virtual void scal(const double alpha) = delete;
 
   /**
    * \brief BLAS AXPY operation.
    * \param alpha The scalar coefficient with which the vector is multiplied
    * \param x     Vector that is to be added.
    */
-  virtual void axpy(const ScalarType alpha, const VectorInterface* x) = 0;
+  virtual void axpy(const double alpha, const VectorInterface* x) = 0;
 
   /**
    * \brief   Computes the scalar products between two vectors.
    * \param   other The second factor.
    * \return        The scalar product.
    */
-  virtual ScalarType dot(const VectorInterface* other) const = 0;
+  virtual double dot(const VectorInterface* other) const = 0;
 
   /**
    * \brief   The l1-norm of the vector.
    * \return  The l1-norm of the vector.
    */
-  virtual ScalarType l1_norm() const = 0;
+  virtual double l1_norm() const = 0;
 
   /**
    * \brief   The l2-norm of the vector.
    * \return  The l2-norm of the vector.
    */
-  virtual ScalarType l2_norm() const = 0;
+  virtual double l2_norm() const = 0;
 
   /**
    * \brief   The l-infintiy-norm of the vector.
    * \return  The l-infintiy-norm of the vector.
    */
-  virtual ScalarType sup_norm() const = 0;
+  virtual double sup_norm() const = 0;
 
   /**
    * \brief Extract components of the vector.
@@ -89,14 +89,14 @@ public:
    * \return                  A Dune::DynamicVector `result` such that `result[i]` is the `component_indices[i]`-th
                               component of the vector.
    */
-  virtual std::vector< ScalarType >* components(const std::vector< size_t >* component_indices) const = 0;
+  virtual std::vector< double >* components(const std::vector< size_t >* component_indices) const = 0;
 
   /**
    * \brief   The maximum absolute value of the vector.
    * \return  A pair< max_ind, max_val >, where max_ind is the index at which the maximum is attained and max_val is the
    *          absolute value.
    */
-  virtual std::pair< size_t, ScalarType > amax() const = 0;
+  virtual std::pair< size_t, double > amax() const = 0;
 
   /**
    * \brief   Adds two vectors.
