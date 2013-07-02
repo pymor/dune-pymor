1516dcc89ab07e00759b67fafd1cc3ef5ecd52be
Felix Albrecht <mail@felixalbrecht.de>
2013-07-02 16:22:35 +0200

    [la.container.interfaces] add proper exceptions

--- dune/pymor/la/container/interfaces.hh
+++ dune/pymor/la/container/interfaces.hh
@@ -12,6 +12,8 @@
 
 #include <dune/common/float_cmp.hh>
 
+#include <dune/pymor/common/exceptions.hh>
+
 namespace Dune {
 namespace Pymor {
 namespace LA {
@@ -25,7 +27,7 @@ public:
 
   /**
    * \brief   A unique (per class, not per instance) identifier.
-   * \return
+   * \return  A unique (per class, not per instance) identifier.
    */
   virtual std::string type() const = 0;
 
@@ -49,7 +51,8 @@ public:
    * \return          Truth value of the comparison.
    */
   virtual bool almost_equal(const VectorInterface* other,
-                            const double epsilon = Dune::FloatCmp::DefaultEpsilon< double >::value()) const = 0;
+                            const double epsilon = Dune::FloatCmp::DefaultEpsilon< double >::value()) const
+    throw (Exception::not_implemented_for_this_combination, Exception::sizes_do_not_match) = 0;
 
   /**
    * \brief BLAS SCAL operation (in-place sclar multiplication).
@@ -62,14 +65,16 @@ public:
    * \param alpha The scalar coefficient with which the vector is multiplied
    * \param x     Vector that is to be added.
    */
-  virtual void axpy(const double alpha, const VectorInterface* x) = 0;
+  virtual void axpy(const double alpha, const VectorInterface* x)
+    throw (Exception::not_implemented_for_this_combination, Exception::sizes_do_not_match) = 0;
 
   /**
    * \brief   Computes the scalar products between two vectors.
    * \param   other The second factor.
    * \return        The scalar product.
    */
-  virtual double dot(const VectorInterface* other) const = 0;
+  virtual double dot(const VectorInterface* other) const
+    throw (Exception::not_implemented_for_this_combination, Exception::sizes_do_not_match) = 0;
 
   /**
    * \brief   The l1-norm of the vector.
@@ -95,7 +100,8 @@ public:
    * \return                  A Dune::DynamicVector `result` such that `result[i]` is the `component_indices[i]`-th
                               component of the vector.
    */
-  virtual std::vector< double > components(const std::vector< int >& component_indices) const = 0;
+  virtual std::vector< double > components(const std::vector< int >& component_indices) const
+    throw (Exception::sizes_do_not_match, Exception::index_out_of_range) = 0;
 
   /**
    * \brief   The maximum absolute value of the vector.
@@ -109,26 +115,30 @@ public:
    * \param   other The right summand.
    * \return        The sum of this and other.
    */
-  virtual VectorInterface* add(const VectorInterface* other) const = 0;
+  virtual VectorInterface* add(const VectorInterface* other) const
+    throw (Exception::not_implemented_for_this_combination, Exception::sizes_do_not_match) = 0;
 
   /**
    * \brief Inplace variant of add().
    * \param other The right summand.
    */
-  virtual void iadd(const VectorInterface* other) = 0;
+  virtual void iadd(const VectorInterface* other)
+    throw (Exception::not_implemented_for_this_combination, Exception::sizes_do_not_match) = 0;
 
   /**
    * \brief   Subtracts two vectors.
    * \param   other The subtrahend.
    * \return        The difference between this and other.
    */
-  virtual VectorInterface* sub(const VectorInterface* other) const = 0;
+  virtual VectorInterface* sub(const VectorInterface* other) const
+    throw (Exception::not_implemented_for_this_combination, Exception::sizes_do_not_match) = 0;
 
   /**
    * \brief Inplace variant of sub().
    * \param other The subtrahend.
    */
-  virtual void isub(const VectorInterface* other) = 0;
+  virtual void isub(const VectorInterface* other)
+    throw (Exception::not_implemented_for_this_combination, Exception::sizes_do_not_match) = 0;
 }; // class VectorInterface
 
 
