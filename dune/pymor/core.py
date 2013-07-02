e7c3fb215d7ee6ac9c32229836de6b9ee2b7ef07
Felix Albrecht <mail@felixalbrecht.de>
2013-07-02 10:17:10 +0200

    [dune.pymor.core] added stl container

--- dune/pymor/core.py
+++ dune/pymor/core.py
@@ -6,4 +6,6 @@ import pybindgen
 def create_module(module_name, include_header):
     module = pybindgen.Module(module_name)
     module.add_include('"' + include_header + '"')
+    module.add_container('std::vector< double >', 'double', 'list')
+    module.add_container('std::vector< int >', 'int', 'list')
     return module
