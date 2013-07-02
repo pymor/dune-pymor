cd20b6ec56611270ac14bf4c82bfc560e9deb0ae
Felix Albrecht <mail@felixalbrecht.de>
2013-07-02 09:31:16 +0200

    [test.la_container] added test of DuneDynamicVector

--- dune/pymor/test/la_container.cc
+++ dune/pymor/test/la_container.cc
@@ -13,24 +13,47 @@
 
 #include <memory>
 
-#include <dune/pymor/la/container/interfaces.hh>
+#include <dune/pymor/la/container/dunedynamic.hh>
 
-#if HAVE_EIGEN
-#include <dune/pymor/la/container/eigen.cc>
+//#if HAVE_EIGEN
+//  #include <dune/pymor/la/container/eigen.hh>
+//#endif // HAVE_EIGEN
 
-typedef testing::Types< Dune::Pymor::LA::EigenDenseVector< double >
+typedef testing::Types< Dune::Pymor::LA::DuneDynamicVector
+//#if HAVE_EIGEN
+//                        Dune::Pymor::LA::EigenDenseVector< double >
+//#endif // HAVE_EIGEN
                       > VectorTypes;
 
 template< class VectorType >
 struct VectorTest
   : public ::testing::Test
 {
-  typedef typename VectorType::ScalarType ScalarType;
   void check() const
   {
     VectorType* vector = VectorType::create(4);
     const std::string DUNE_UNUSED(type) = vector->type();
     const int DUNE_UNUSED(dim) = vector->dim();
+    const bool DUNE_UNUSED(almost_equal) = vector->almost_equal(vector);
+    vector->scal(1.0);
+    vector->axpy(1.0, vector);
+    const double DUNE_UNUSED(dot) = vector->dot(vector);
+    const double DUNE_UNUSED(l1_norm) = vector->l1_norm();
+    const double DUNE_UNUSED(l2_norm) = vector->l2_norm();
+    const double DUNE_UNUSED(sup_norm) = vector->sup_norm();
+    std::vector< int >* component_indices = new std::vector< int >(2);
+    component_indices->operator[](0) = 3;
+    component_indices->operator[](1) = 1;
+    std::vector< double >* components = vector->components(component_indices);
+    delete component_indices;
+    delete components;
+    std::pair< int, double >* DUNE_UNUSED(amax) = vector->amax();
+    VectorType* add = vector->add(vector);
+    delete add;
+    vector->iadd(vector);
+    VectorType* sub = vector->sub(vector);
+    delete sub;
+    vector->isub(vector);
     delete vector;
   }
 }; // struct VectorTest
@@ -47,12 +70,3 @@ int main(int argc, char** argv)
   test_init(argc, argv);
   return RUN_ALL_TESTS();
 }
-
-#else // HAVE_EIGEN
-
-int main(int /*argc*/, char** /*argv*/)
-{
-  return 0;
-}
-
-#endif // HAVE_EIGEN
