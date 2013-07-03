bc029c394093ce01a6f39246b437d60452d0208d
Felix Albrecht <mail@felixalbrecht.de>
2013-07-03 22:39:32 +0200

    moved dune-pymor.common -> dune.pymor.common.exceptions

---
+++ dune/pymor/common/exceptions.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+
+# This file is part of the dune-pymor project:
+#   https://github.com/pyMor/dune-pymor
+# Copyright Holders: Felix Albrecht, Stephan Rave
+# License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+import pybindgen
+
+
+class Exceptions():
+
+    def __init__(self,
+                 PymorException,
+                 types_are_not_compatible,
+                 sizes_do_not_match,
+                 index_out_of_range):
+        self.PymorException = PymorException
+        self.types_are_not_compatible = types_are_not_compatible
+        self.sizes_do_not_match = sizes_do_not_match
+        self.index_out_of_range = index_out_of_range
+
+
+def add_Exceptions(module):
+    assert(isinstance(module, pybindgen.module.Module))
+    namespace = module.add_cpp_namespace('Dune')
+    PymorException = namespace.add_exception('Dune::PymorException')
+    namespace = namespace.add_cpp_namespace('Pymor').add_cpp_namespace('Exception')
+    types_are_not_compatible = namespace.add_exception('types_are_not_compatible',
+                                                       parent=PymorException,
+                                                       message_rvalue='%(EXC)s.pymorWhat()')
+    sizes_do_not_match = namespace.add_exception('sizes_do_not_match',
+                                                 parent=PymorException,
+                                                 message_rvalue='%(EXC)s.pymorWhat()')
+    index_out_of_range = namespace.add_exception('index_out_of_range',
+                                                 parent=PymorException,
+                                                 message_rvalue='%(EXC)s.pymorWhat()')
+    return module, Exceptions(PymorException,
+                              types_are_not_compatible,
+                              sizes_do_not_match,
+                              index_out_of_range)
