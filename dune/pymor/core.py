da18723c7415aab4af5157b72b470d106249d862
Felix Albrecht <mail@felixalbrecht.de>
2013-07-04 19:08:55 +0200

    [dune.pymor.core] added ParameterType

--- dune/pymor/core.py
+++ dune/pymor/core.py
@@ -8,14 +8,19 @@
 import pybindgen
 
 from dune.pymor.common import add_Exceptions
+from dune.pymor.parameters.base import add_ParameterType
 
 
-def create_module(module_name, include_header):
+def create_module(module_name, include_header, add_all_of_dune_pymor=True):
     module = pybindgen.Module(module_name)
     module.add_include('"' + include_header + '"')
     # add stl container
+    module.add_container('std::vector< std::string >', 'std::string', 'list')
     module.add_container('std::vector< double >', 'double', 'list')
     module.add_container('std::vector< int >', 'int', 'list')
     # add dnue-pymor
     module, exceptions = add_Exceptions(module)
+    if add_all_of_dune_pymor:
+        module, _ = add_ParameterType(module, exceptions)
+#        module, _ = add_Parameter(module, exceptions)
     return module, exceptions
