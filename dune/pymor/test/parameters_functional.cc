5125e5693fdb5ae61d36e670e8127549ef8dea73
Felix Albrecht <mail@felixalbrecht.de>
2013-07-05 14:17:31 +0200

    [test.parameters_functional] update

--- dune/pymor/test/parameters_functional.cc
+++ dune/pymor/test/parameters_functional.cc
@@ -21,11 +21,23 @@ using namespace Dune::Pymor;
 
 TEST(Functional, Parameters_Functional)
 {
-  ParameterFunctional theta1("mu[0] + sin(mu[1]) + cos(mu[2])");
-  double res = theta1.evaluate({1.0, 0.0, 0.0});
+  const Parameter mu1 = {"diffusion", {1.0, 0.0, 0.0}};
+  const ParameterFunctional theta1(mu1.type(), "diffusion[0] + sin(diffusion[1]) + cos(diffusion[2])");
+  std::string ss = theta1.report("theta1");
+//  std::cout << ss << std::endl;
+  double res = theta1.evaluate(mu1);
   if (!Dune::FloatCmp::eq(res, 2.0)) DUNE_PYMOR_THROW(PymorException, "");
-  std::string exp = theta1;
-  if (exp != "mu[0] + sin(mu[1]) + cos(mu[2])") DUNE_PYMOR_THROW(PymorException, "");
+  std::string exp = theta1.expression();
+  if (exp != "diffusion[0] + sin(diffusion[1]) + cos(diffusion[2])") DUNE_PYMOR_THROW(PymorException, "");
+
+  const Parameter mu2 = {{"diffusion", "force"}, {{1.0}, {0.0, 0.0}}};
+  const ParameterFunctional theta2(mu2.type(), "diffusion + sin(force[0]) + exp(force[1])");
+  ss = theta2.report("theta2");
+//  std::cout << ss << std::endl;
+  res = theta2.evaluate(mu2);
+  if (!Dune::FloatCmp::eq(res, 2.0)) DUNE_PYMOR_THROW(PymorException, "");
+  exp = theta2.expression();
+  if (exp != "diffusion + sin(force[0]) + exp(force[1])") DUNE_PYMOR_THROW(PymorException, "");
 }
 
 int main(int argc, char** argv)
