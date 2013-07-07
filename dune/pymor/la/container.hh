97a9cf4e579d5f668777fad3f8b1752fb4f706c4
Felix Albrecht <mail@felixalbrecht.de>
2013-07-07 22:53:45 +0200

    [la.container] seperated into .hh and .cc

--- dune/pymor/la/container.hh
+++ dune/pymor/la/container.hh
@@ -15,39 +15,11 @@ namespace Dune {
 namespace Pymor {
 namespace LA {
 
-
-std::vector< std::string > availableVectors()
-{
-  return {
-        DuneDynamicVector::static_type()
-#if HAVE_EIGEN
-      , EigenDenseVector::static_type()
-#endif
-  };
-} // ... availableVectors(...)
-
+std::vector< std::string > availableVectors();
 
 VectorInterface* createVector(const std::string type,
                               const size_t size,
-                              const value = 0.0) throw (Exception::types_are_not_compatible)
-{
-  if (type == DuneDynamicVector::static_type())
-    return new DuneDynamicVector(size, value);
-#if HAVE_EIGEN
-  else if (type == EigenDenseVector::static_type())
-    return new EigenDenseVector(size, value);
-#endif
-  else {
-    std::stringstream msg;
-    msg << "type (" << type << ") is not any of {";
-    const auto types = availableVectors();
-    for (size_t ii = 0; ii < (types.size() - 1); ++ii)
-      msg << types[ii] << ", ";
-    msg << types[types.size() - 1] << "}!";
-    DUNE_PYMOR_THROW(Exception::types_are_not_compatible, msg.str());
-  }
-} // ... createVector(...)
-
+                              const double value = 0.0) throw (Exception::types_are_not_compatible);
 
 } // namespace LA
 } // namespace Pymor
