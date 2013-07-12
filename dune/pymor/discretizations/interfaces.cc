bd53d3860672289f6956af3c8f358711b0f898b1
Felix Albrecht <mail@felixalbrecht.de>
2013-07-12 23:51:56 +0200

    [discretizations] added StationaryDiscretizationInterface

---
+++ dune/pymor/discretizations/interfaces.cc
@@ -0,0 +1,37 @@
+// This file is part of the dune-pymor project:
+//   https://github.com/pyMor/dune-pymor
+// Copyright Holders: Felix Albrecht, Stephan Rave
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#include "interfaces.hh"
+
+namespace Dune {
+namespace Pymor {
+
+
+StationaryDiscretizationInterface::StationaryDiscretizationInterface()
+  : Parametric()
+{}
+
+StationaryDiscretizationInterface::StationaryDiscretizationInterface(const ParameterType& tt)
+  : Parametric(tt)
+{}
+
+StationaryDiscretizationInterface::StationaryDiscretizationInterface(const std::string& kk,
+                                                                     const int& vv)
+  throw (Exception::key_is_not_valid, Exception::index_out_of_range)
+  : Parametric(kk, vv)
+{}
+
+StationaryDiscretizationInterface::StationaryDiscretizationInterface(const std::vector< std::string >& kk,
+                                                                     const std::vector< int >& vv)
+  throw (Exception::key_is_not_valid, Exception::index_out_of_range, Exception::sizes_do_not_match)
+  : Parametric(kk, vv)
+{}
+
+StationaryDiscretizationInterface::~StationaryDiscretizationInterface()
+{}
+
+
+} // namespace Pymor
+} // namespace Dune
---
+++ dune/pymor/discretizations/interfaces.hh
@@ -0,0 +1,60 @@
+// This file is part of the dune-pymor project:
+//   https://github.com/pyMor/dune-pymor
+// Copyright Holders: Felix Albrecht, Stephan Rave
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#ifndef DUNE_PYMOR_DISCRETIZATIONS_INTERFACES_HH
+#define DUNE_PYMOR_DISCRETIZATIONS_INTERFACES_HH
+
+#include <vector>
+#include <string>
+
+#include <dune/pymor/parameters/base.hh>
+#include <dune/pymor/common/exceptions.hh>
+#include <dune/pymor/la/container/interfaces.hh>
+#include <dune/pymor/operators/interfaces.hh>
+
+namespace Dune {
+namespace Pymor {
+
+
+class StationaryDiscretizationInterface
+  : public Parametric
+{
+public:
+  StationaryDiscretizationInterface();
+
+  StationaryDiscretizationInterface(const ParameterType& tt);
+
+  StationaryDiscretizationInterface(const std::string& kk, const int& vv) throw (Exception::key_is_not_valid,
+                                                                                 Exception::index_out_of_range);
+
+  StationaryDiscretizationInterface(const std::vector< std::string >& kk,
+                                    const std::vector< int >& vv) throw (Exception::key_is_not_valid,
+                                                                         Exception::index_out_of_range,
+                                                                         Exception::sizes_do_not_match);
+
+  virtual ~StationaryDiscretizationInterface();
+
+  virtual std::vector< std::string > available_operators() const = 0;
+
+  virtual OperatorInterface* get_operator(const std::string id) const = 0;
+
+  virtual LA::VectorInterface* create_vector() const = 0;
+
+  virtual std::vector< std::string > solver_options() const = 0;
+
+  virtual std::string solver_options(std::string context) const = 0;
+
+  virtual void solve(LA::VectorInterface* vector, const Parameter mu = Parameter()) const = 0;
+
+  virtual void visualize(const LA::VectorInterface* vector,
+                         const std::string filename,
+                         const std::string name) const = 0;
+}; // class StationaryDiscretizationInterface
+
+
+} // namespace Pymor
+} // namespace Dune
+
+#endif // DUNE_PYMOR_DISCRETIZATIONS_INTERFACES_HH
