bc09983d3081daf8c505ee5df77ee490d06ef0b9
Felix Albrecht <mail@felixalbrecht.de>
2013-07-16 00:29:51 +0200

    added config.h to .ccs

--- dune/pymor/discretizations/interfaces.cc
+++ dune/pymor/discretizations/interfaces.cc
@@ -3,6 +3,12 @@
 // Copyright Holders: Felix Albrecht, Stephan Rave
 // License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
+#ifdef HAVE_CMAKE_CONFIG
+  #include "cmake_config.h"
+#elif defined (HAVE_CONFIG_H)
+  #include "config.h"
+#endif // HAVE_CMAKE_CONFIG
+
 #include "interfaces.hh"
 
 namespace Dune {
