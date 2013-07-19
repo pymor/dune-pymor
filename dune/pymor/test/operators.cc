280489f4ba4daea774e5b2e6d9fd7d4e5fdee5a2
Felix Albrecht <mail@felixalbrecht.de>
2013-07-19 13:20:12 +0200

    [test.opearators] updated

--- dune/pymor/test/operators.cc
+++ dune/pymor/test/operators.cc
@@ -10,176 +10,213 @@
 #endif // HAVE_CMAKE_CONFIG
 
 #include <utility>
+#include <type_traits>
 
 #include <dune/stuff/test/test_common.hh>
 
 #include <dune/common/float_cmp.hh>
-#include <dune/common/typetraits.hh>
 
 #include <dune/pymor/common/exceptions.hh>
-#include <dune/pymor/operators/default.hh>
 #include <dune/pymor/parameters/base.hh>
 #include <dune/pymor/parameters/functional.hh>
-#include <dune/pymor/operators/dunedynamicmatrix.hh>
-#if HAVE_EIGEN
-  #include <dune/pymor/operators/eigen.hh>
-#endif
-#include <dune/pymor/operators.hh>
+#include <dune/pymor/la/container/dunedynamic.hh>
+#include <dune/pymor/la/container.hh>
+#include <dune/pymor/operators/interfaces.hh>
+#include <dune/pymor/operators/affine.hh>
+#include <dune/pymor/operators/dunedynamic.hh>
+//#if HAVE_EIGEN
+//  #include <dune/pymor/operators/eigen.hh>
+//#endif
+//#include <dune/pymor/operators.hh>
 
 
 using namespace Dune;
 using namespace Dune::Pymor;
 
+static const size_t dim = 4;
+
 typedef testing::Types<
-                        std::pair< Dune::Pymor::Operators::DuneDynamicMatrix,
-                                   Dune::Pymor::LA::DuneDynamicVector >
-#if HAVE_EIGEN
-                      , std::pair< Dune::Pymor::Operators::EigenDenseMatrix,
-                                   Dune::Pymor::LA::EigenDenseVector >
-                      , std::pair< Dune::Pymor::Operators::EigenRowMajorSparseMatrix,
-                                   Dune::Pymor::LA::EigenDenseVector >
-#endif // HAVE_EIGEN
-                      > LinearOperatorTypes;
+                        Dune::Pymor::Operators::DuneDynamic< double >
+//#if HAVE_EIGEN
+//                      , std::pair< Dune::Pymor::Operators::EigenDenseMatrix,
+//                                   Dune::Pymor::LA::EigenDenseVector >
+//                      , std::pair< Dune::Pymor::Operators::EigenRowMajorSparseMatrix,
+//                                   Dune::Pymor::LA::EigenDenseVector >
+//#endif // HAVE_EIGEN
+                      > ContainerBasedOperatorTypes;
 
 
-template< class TypePair >
-struct OperatorTest
+template< class OperatorType >
+struct ContainerBasedOperatorTest
   : public ::testing::Test
 {
   void check() const
   {
-    typedef typename TypePair::first_type   OperatorType;
-    typedef typename TypePair::second_type  VectorType;
-    static_assert(Dune::IsBaseOf< OperatorInterface, OperatorType >::value,
-                  "not an operator");
-    const size_t dim = 2;
-    VectorType* U = new VectorType(dim, 1.0);
-    VectorType* V = new VectorType(dim, 1.0);
-    OperatorType* op = createLinearOperator(OperatorType(), dim, true);
-    if (op->parametric()) DUNE_PYMOR_THROW(PymorException, "");
-    if (!op->parameter_type().empty()) DUNE_PYMOR_THROW(PymorException, "");
-    if (op->dim_source() != dim) DUNE_PYMOR_THROW(PymorException, "");
-    if (op->dim_range() != dim) DUNE_PYMOR_THROW(PymorException, "");
-    if (op->type_source() != U->type()) DUNE_PYMOR_THROW(PymorException, "");
-    if (op->type_range() != U->type()) DUNE_PYMOR_THROW(PymorException, "");
-    op->apply(U, V);
-    if (V->dim() != op->dim_range()) DUNE_PYMOR_THROW(PymorException, "");
-    if (!Dune::FloatCmp::eq(V->components({0})[0], 1.0) || !Dune::FloatCmp::eq(V->components({1})[0], 1.0))
-      DUNE_PYMOR_THROW(PymorException, "");
-    const double res = op->apply2(U, V);
-    if (!Dune::FloatCmp::eq(res, 2.0)) DUNE_PYMOR_THROW(PymorException, "");
-    // check inverse
-    const std::vector< std::string > invert_options = op->invert_options();
-    if (invert_options.size() == 0) DUNE_PYMOR_THROW(PymorException, "");
-    for (auto option : invert_options) {
-      const auto inverseOp = op->invert(option);
-      try {
-        inverseOp->apply(V, U);
-        if (U->dim() != inverseOp->dim_range()) DUNE_PYMOR_THROW(PymorException, "");
-        if (!Dune::FloatCmp::eq(U->components({0})[0], 1.0) || !Dune::FloatCmp::eq(U->components({1})[0], 1.0))
-          DUNE_PYMOR_THROW(PymorException, "");
-        op->apply_inverse(V, U, option);
-        if (U->dim() != op->dim_source()) DUNE_PYMOR_THROW(PymorException, "");
-        if (!Dune::FloatCmp::eq(U->components({0})[0], 1.0) || !Dune::FloatCmp::eq(U->components({1})[0], 1.0))
-          DUNE_PYMOR_THROW(PymorException, "");
-      } catch (Dune::Pymor::Exception::linear_solver_failed&) {}
-      delete inverseOp;
+    // static tests
+    typedef typename OperatorType::Traits         Traits;
+    // * of the traits
+    typedef typename Traits::derived_type T_derived_type;
+    static_assert(std::is_same< OperatorType, T_derived_type >::value,
+                  "OperatorType::Traits::derived_type is wrong!");
+    typedef typename Traits::SourceType   T_SourceType;
+    typedef typename Traits::RangeType    T_RangeType;
+    typedef typename Traits::ScalarType   T_ScalarType;
+    typedef typename Traits::InverseType  T_InverseType;
+    // * of the class itself (aka the derived type)
+    typedef typename OperatorType::SourceType     D_SourceType;
+    typedef typename OperatorType::RangeType      D_RangeType;
+    typedef typename OperatorType::ScalarType     D_ScalarType;
+    typedef typename OperatorType::InverseType    D_InverseType;
+    typedef typename OperatorType::ContainerType  D_ContainerType;
+    // * of the class as the operator interface
+    typedef OperatorInterface< Traits >         InterfaceType;
+    static_assert(std::is_base_of< OperatorInterface< Traits >, OperatorType >::value,
+                  "OperatorType has to be derived from OperaotrInterface is wrong!");
+    typedef typename InterfaceType::SourceType  I_SourceType;
+    typedef typename InterfaceType::RangeType   I_RangeType;
+    typedef typename InterfaceType::ScalarType  I_ScalarType;
+    typedef typename InterfaceType::InverseType D_InverseType;
+    // * of the class as the container interface
+    static_assert(std::is_base_of< LA::ProvidesContainer< Traits >, OperatorType >::value,
+                  "OperatorType has to be derived from LA::ProvidesContainer!");
+    // dynamic tests
+    // * of the class itself (aka the derived type)
+    OperatorType d_from_ptr(new D_ContainerType(LA::createContainer(D_ContainerType(), dim)));
+    OperatorType d_from_shared_ptr(std::make_shared< D_ContainerType >(LA::createContainer(D_ContainerType(), dim)));
+    d_from_shared_ptr = d_from_ptr;
+    OperatorType DUNE_UNUSED(d_copy_constructor)(d_from_ptr); // <- at this point, all operators share the same matrix!
+    const bool d_linear = d_from_ptr.linear();
+    if (!d_linear) DUNE_PYMOR_THROW(PymorException, "");
+    if (d_from_ptr.parametric()) DUNE_PYMOR_THROW(PymorException, "");
+    const unsigned int d_dim_source = d_from_ptr.dim_source();
+    if (d_dim_source != dim) DUNE_PYMOR_THROW(PymorException, d_dim_source);
+    const unsigned int d_dim_range = d_from_ptr.dim_range();
+    if (d_dim_range != dim) DUNE_PYMOR_THROW(PymorException, d_dim_range);
+    D_SourceType source = LA::createContainer(D_SourceType(), dim);
+    D_SourceType range = d_from_ptr.apply(source);
+    d_from_ptr.apply(source, range);
+    D_ScalarType DUNE_UNUSED(d_apply2) = d_from_ptr.apply2(range, source);
+    std::vector< std::string > d_invert_options = d_from_ptr.invert_options();
+    D_InverseType d_inverse = d_from_ptr.invert(d_invert_options[0]);
+    d_inverse.apply(range, source);
+    d_from_ptr.apply_inverse(source, range, d_invert_options[0]);
+  }
+}; // struct ContainerBasedOperatorTest
+
+
+TYPED_TEST_CASE(ContainerBasedOperatorTest, ContainerBasedOperatorTypes);
+TYPED_TEST(ContainerBasedOperatorTest, OPERATORS) {
+  this->check();
+}
+
+
+template< class OperatorImp >
+struct LinearAffinelyDecomposedContainerBasedOperatorTest
+  : public ::testing::Test
+{
+  void check() const
+  {
+    // static tests
+    typedef Operators::LinearAffinelyDecomposedContainerBased< OperatorImp > OperatorType;
+    typedef typename OperatorType::Traits Traits;
+    // * of the traits
+    typedef typename Traits::derived_type T_derived_type;
+    static_assert(std::is_same< OperatorType, T_derived_type >::value,
+                  "OperatorType::Traits::derived_type is wrong!");
+    typedef typename Traits::ComponentType ComponentType;
+    // * of the class itself (aka the derived type)
+    typedef typename OperatorType::ComponentType  D_ComponentType;
+    typedef typename OperatorType::FrozenType     D_FrozenType;
+    typedef typename OperatorType::ScalarType     D_ScalarType;
+    typedef typename OperatorType::SourceType     D_SourceType;
+    typedef typename OperatorType::RangeType      D_RangeType;
+    typedef typename OperatorType::InverseType    D_InverseType;
+    // * of the class as the interface
+    typedef AffinelyDecomposedOperatorInterface< Traits > InterfaceType;
+    typedef typename InterfaceType::derived_type I_derived_type;
+    static_assert(std::is_same< OperatorType, I_derived_type >::value,
+                  "InterfaceType::derived_type is wrong!");
+    typedef typename InterfaceType::ComponentType I_ComponentType;
+    typedef typename InterfaceType::FrozenType    I_FrozenType;
+    typedef typename InterfaceType::ScalarType    I_ScalarType;
+    typedef typename InterfaceType::SourceType    I_SourceType;
+    typedef typename InterfaceType::RangeType     I_RangeType;
+    typedef typename InterfaceType::InverseType   I_InverseType;
+    // dynamic tests
+    // * of the class itself (aka the derived type)
+    typedef typename OperatorImp::ContainerType MatrixType;
+    typedef LA::AffinelyDecomposedConstContainer< MatrixType > AffinelyDecomposedMatrixType;
+    AffinelyDecomposedMatrixType affinelyDecomposedMatrix(new MatrixType(LA::createContainer(MatrixType(), dim)));
+    affinelyDecomposedMatrix.register_component(new MatrixType((LA::createContainer(MatrixType(), dim))),
+                                                new ParameterFunctional("diffusion", 1, "diffusion[0]"));
+    affinelyDecomposedMatrix.register_component(new MatrixType((LA::createContainer(MatrixType(), dim))),
+                                                new ParameterFunctional("force", 2, "force[0]"));
+    const Parameter mu = {{"diffusion", "force"},
+                          {{1.0}, {1.0, 1.0}}};
+    if (affinelyDecomposedMatrix.parameter_type() != mu.type())
+      DUNE_PYMOR_THROW(PymorException,
+                       "\nmu.type()                                 = " << mu.type()
+                       << "\naffinelyDecomposedMatrix.parameter_type() = "
+                       << affinelyDecomposedMatrix.parameter_type());
+    OperatorType d_operator(affinelyDecomposedMatrix);
+    if (!d_operator.parametric()) DUNE_PYMOR_THROW(PymorException, "");
+    if (d_operator.parameter_type() != mu.type())
+      DUNE_PYMOR_THROW(PymorException,
+                       "\nmu.type()                   = " << mu.type()
+                       << "\nd_operator.parameter_type() = " << d_operator.parameter_type());
+    const unsigned int d_num_components = d_operator.num_components();
+    if (d_num_components != 2) DUNE_PYMOR_THROW(PymorException, d_num_components);
+    for (unsigned int qq = 0; qq < d_num_components; ++qq) {
+      D_ComponentType component = d_operator.component(qq);
+      if (component.parametric()) DUNE_PYMOR_THROW(PymorException, "");
+      ParameterFunctional DUNE_UNUSED(coefficient) = d_operator.coefficient(qq);
     }
-    delete V;
-    delete op;
-    delete U;
+    if (!d_operator.has_affine_part()) DUNE_PYMOR_THROW(PymorException, "");
+    D_ComponentType d_affine_part = d_operator.affine_part();
+    if (d_affine_part.parametric()) DUNE_PYMOR_THROW(PymorException, "");
+    D_FrozenType d_frozen = d_operator.freeze_parameter(mu);
+    if (d_frozen.parametric()) DUNE_PYMOR_THROW(PymorException, "");
+    D_SourceType d_source(dim, D_ScalarType(1));
+    D_RangeType d_range = d_operator.apply(d_source, mu);
+    D_RangeType d_frozen_apply = d_frozen.apply(d_source);
+    if (!d_range.almost_equal(d_frozen_apply)) DUNE_PYMOR_THROW(PymorException, "");
+    std::vector< std::string > d_invert_options = d_operator.invert_options();
+    D_InverseType d_inverse = d_operator.invert(d_invert_options[0], mu);
+    d_inverse.apply(d_range, d_source);
+    d_operator.apply_inverse(d_source, d_range, d_invert_options[0], mu);
+    // * of the class as the interface
+    InterfaceType& i_operator = static_cast< InterfaceType& >(d_operator);
+    if (!i_operator.parametric()) DUNE_PYMOR_THROW(PymorException, "");
+    if (i_operator.parameter_type() != mu.type())
+      DUNE_PYMOR_THROW(PymorException,
+                       "\nmu.type()                   = " << mu.type()
+                       << "\ni_operator.parameter_type() = " << i_operator.parameter_type());
+    const unsigned int i_num_components = i_operator.num_components();
+    if (i_num_components != 2) DUNE_PYMOR_THROW(PymorException, i_num_components);
+    for (unsigned int qq = 0; qq < i_num_components; ++qq) {
+      I_ComponentType component = i_operator.component(qq);
+      if (component.parametric()) DUNE_PYMOR_THROW(PymorException, "");
+      ParameterFunctional DUNE_UNUSED(coefficient) = i_operator.coefficient(qq);
+    }
+    if (!i_operator.has_affine_part()) DUNE_PYMOR_THROW(PymorException, "");
+    I_ComponentType i_affine_part = i_operator.affine_part();
+    if (i_affine_part.parametric()) DUNE_PYMOR_THROW(PymorException, "");
+    I_FrozenType i_frozen = i_operator.freeze_parameter(mu);
+    if (i_frozen.parametric()) DUNE_PYMOR_THROW(PymorException, "");
+    I_SourceType i_source(dim, I_ScalarType(1));
+    I_RangeType i_range = i_operator.apply(i_source, mu);
+    I_RangeType i_frozen_apply = i_frozen.apply(i_source);
+    if (!i_range.almost_equal(i_frozen_apply)) DUNE_PYMOR_THROW(PymorException, "");
+    std::vector< std::string > i_invert_options = i_operator.invert_options();
+    I_InverseType i_inverse = i_operator.invert(i_invert_options[0], mu);
+    i_inverse.apply(i_range, i_source);
+    i_operator.apply_inverse(i_source, i_range, i_invert_options[0], mu);
   }
-}; // struct OperatorTest
+}; // struct LinearAffinelyDecomposedContainerBasedOperatorTest
 
 
-TYPED_TEST_CASE(OperatorTest, LinearOperatorTypes);
-TYPED_TEST(OperatorTest, OPERATORS) {
-  this->check();
-}
-
-
-template< class TypePair >
-struct LinearOperatorTest
-  : public ::testing::Test
-{
-  void check() const
-  {
-    typedef typename TypePair::first_type   OperatorType;
-    typedef typename TypePair::second_type  VectorType;
-    static_assert(Dune::IsBaseOf< LinearOperatorInterface, OperatorType >::value,
-                  "not a linear operator");
-    const size_t dim = 2;
-    VectorType* U = new VectorType(dim, 1.0);
-    VectorType* V = new VectorType(dim, 1.0);
-    OperatorType* op = createLinearOperator(OperatorType(), dim, true);
-    if (!op->linear()) DUNE_PYMOR_THROW(PymorException, "");
-    OperatorType* op2 = op->copy();
-    op->scal(1.0);
-    op->axpy(1.0, op2);
-    op->apply(U, V);
-    if (V->dim() != op->dim_range()) DUNE_PYMOR_THROW(PymorException, "");
-    if (!Dune::FloatCmp::eq(V->components({0})[0], 2.0) || !Dune::FloatCmp::eq(V->components({1})[0], 2.0))
-      DUNE_PYMOR_THROW(PymorException, "");
-    delete op2;
-    delete op;
-    delete V;
-    delete U;
-  }
-}; // struct LinearOperatorTest
-
-
-TYPED_TEST_CASE(LinearOperatorTest, LinearOperatorTypes);
-TYPED_TEST(LinearOperatorTest, OPERATORS) {
-  this->check();
-}
-
-
-template< class TypePair >
-struct LinearAffinelyDecomposedDefaultTest
-  : public ::testing::Test
-{
-  void check() const
-  {
-    typedef typename TypePair::first_type   OperatorType;
-    typedef typename TypePair::second_type  VectorType;
-    typedef Operators::LinearAffinelyDecomposedDefault< OperatorType > AffineparametricOperatorType;
-    const Parameter mu = {"diffusion", {1.0, 1.0}};
-    const size_t dim = 2;
-    AffineparametricOperatorType op(createLinearOperator(OperatorType(), dim));
-    op.register_component(createLinearOperator(OperatorType(), dim),
-                          new ParameterFunctional(mu.type(), "diffusion[0]"));
-    op.register_component(createLinearOperator(OperatorType(), dim),
-                          new ParameterFunctional(mu.type(), "diffusion[1]"));
-    VectorType* U = new VectorType(dim, 1.0);
-    VectorType* V = new VectorType(dim, 1.0);
-    if (!op.linear()) DUNE_PYMOR_THROW(PymorException, "");
-    if (!op.parametric()) DUNE_PYMOR_THROW(PymorException, "");
-    if (op.parameter_type() != mu.type()) DUNE_PYMOR_THROW(PymorException, "");
-    if (op.dim_source() != dim) DUNE_PYMOR_THROW(PymorException, "");
-    if (op.dim_range() != dim) DUNE_PYMOR_THROW(PymorException, "");
-    if (op.type_source() != U->type()) DUNE_PYMOR_THROW(PymorException, "");
-    if (op.type_range() != U->type()) DUNE_PYMOR_THROW(PymorException, "");
-    op.apply(U, V, mu);
-    if (V->dim() != op.dim_range()) DUNE_PYMOR_THROW(PymorException, "");
-    if (!Dune::FloatCmp::eq(V->components({0})[0], 3.0) || !Dune::FloatCmp::eq(V->components({1})[0], 3.0))
-      DUNE_PYMOR_THROW(PymorException, "");
-    const double res = op.apply2(U, V, mu);
-    if (!Dune::FloatCmp::eq(res, 18.0)) DUNE_PYMOR_THROW(PymorException, "");
-    OperatorType* frozenOp = op.freeze_parameter(mu);
-    if (frozenOp->parametric()) DUNE_PYMOR_THROW(PymorException, "");
-    frozenOp->apply(U, V);
-    if (V->dim() != op.dim_range()) DUNE_PYMOR_THROW(PymorException, "");
-    if (!Dune::FloatCmp::eq(V->components({0})[0], 3.0) || !Dune::FloatCmp::eq(V->components({1})[0], 3.0))
-      DUNE_PYMOR_THROW(PymorException, "");
-    delete frozenOp;
-    delete V;
-    delete U;
-  }
-}; // struct LinearAffinelyDecomposedDefaultTest
-
-
-TYPED_TEST_CASE(LinearAffinelyDecomposedDefaultTest, LinearOperatorTypes);
-TYPED_TEST(LinearAffinelyDecomposedDefaultTest, OPERATORS) {
+TYPED_TEST_CASE(LinearAffinelyDecomposedContainerBasedOperatorTest, ContainerBasedOperatorTypes);
+TYPED_TEST(LinearAffinelyDecomposedContainerBasedOperatorTest, OPERATORS) {
   this->check();
 }
 
