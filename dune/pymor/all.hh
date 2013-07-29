820f6f87c7b738b1e7b6cf4c2a79e3e4eb2f568f
Felix Albrecht <mail@felixalbrecht.de>
2013-07-29 16:07:34 +0200

    [all] added header for python bindings

---
+++ dune/pymor/all.hh
@@ -0,0 +1,25 @@
+// This file is part of the dune-pymor project:
+//   https://github.com/pyMor/dune-pymor
+// Copyright Holders: Felix Albrecht, Stephan Rave
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#ifndef DUNE_PYMOR_ALL_HH
+#define DUNE_PYMOR_ALL_HH
+
+#include <dune/pymor/discretizations/interfaces.hh>
+#include <dune/pymor/functionals/affine.hh>
+#include <dune/pymor/functionals/default.hh>
+#include <dune/pymor/functionals/interfaces.hh>
+#include <dune/pymor/la/container.hh>
+#include <dune/pymor/la/container/affine.hh>
+#include <dune/pymor/la/container/dunedynamic.hh>
+#include <dune/pymor/la/container/eigen.hh>
+#include <dune/pymor/la/container/interfaces.hh>
+#include <dune/pymor/operators/affine.hh>
+#include <dune/pymor/operators/dunedynamic.hh>
+#include <dune/pymor/operators/eigen.hh>
+#include <dune/pymor/operators/interfaces.hh>
+#include <dune/pymor/parameters/base.hh>
+#include <dune/pymor/parameters/functional.hh>
+
+#endif // DUNE_PYMOR_ALL_HH
