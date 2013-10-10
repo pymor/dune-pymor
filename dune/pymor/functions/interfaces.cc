f4f97d9d06bfbdebdeb9460dbc85b1c3770e1795
Felix Albrecht <mail@felixalbrecht.de>
2013-10-10 22:06:29 +0200

    [functions.interfaces] apply changes from dune-stuff

--- dune/pymor/functions/interfaces.cc
+++ dune/pymor/functions/interfaces.cc
@@ -3,14 +3,6 @@
 // Copyright Holders: Felix Albrecht, Stephan Rave
 // License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
-#ifdef HAVE_CMAKE_CONFIG
-  #include "cmake_config.h"
-#elif defined (HAVE_CONFIG_H)
-  #include "config.h"
-#endif // HAVE_CMAKE_CONFIG
-
-#include "interfaces.hh"
-
 namespace Dune {
 namespace Pymor {
 
@@ -18,55 +10,34 @@ namespace Pymor {
 // =======================================
 // ===== ParametricFunctionInterface =====
 // =======================================
-template< class D, int d, class R, int rR, int rC >
-ParametricFunctionInterface< D, d, R, rR, rC >::ParametricFunctionInterface(const ParameterType tt)
+template< class E, class D, int d, class R, int r, int rC >
+ParametricFunctionInterface< E, D, d, R, r, rC >::ParametricFunctionInterface(const ParameterType tt)
   : Parametric(tt)
 {}
 
-template< class D, int d, class R, int rR, int rC >
-ParametricFunctionInterface< D, d, R, rR, rC >::ParametricFunctionInterface(const Parametric& other)
+template< class E, class D, int d, class R, int r, int rC >
+ParametricFunctionInterface< E, D, d, R, r, rC >::ParametricFunctionInterface(const Parametric& other)
   : Parametric(other)
 {}
 
-template< class D, int d, class R, int rR, int rC >
-ParametricFunctionInterface< D, d, R, rR, rC >::~ParametricFunctionInterface()
+template< class E, class D, int d, class R, int r, int rC >
+ParametricFunctionInterface< E, D, d, R, r, rC >::~ParametricFunctionInterface()
 {}
 
-template< class D, int d, class R, int rR, int rC >
-std::string ParametricFunctionInterface< D, d, R, rR, rC >::static_id()
+template< class E, class D, int d, class R, int r, int rC >
+std::string ParametricFunctionInterface< E, D, d, R, r, rC >::static_id()
 {
   return "dune.pymor.parametricfunction";
 }
 
-template< class D, int d, class R, int rR, int rC >
-std::string ParametricFunctionInterface< D, d, R, rR, rC >::name() const
+template< class E, class D, int d, class R, int r, int rC >
+std::string ParametricFunctionInterface< E, D, d, R, r, rC >::name() const
 {
   return "dune.pymor.parametricfunction";
 }
 
-template< class D, int d, class R, int rR, int rC >
-int ParametricFunctionInterface< D, d, R, rR, rC >::order() const
-{
-  return -1;
-}
-
-template< class D, int d, class R, int rR, int rC >
-typename ParametricFunctionInterface< D, d, R, rR, rC >::RangeType
-ParametricFunctionInterface< D, d, R, rR, rC >::evaluate(const DomainType& x,
-                                                         const Parameter mu) const
-  throw (Exception::wrong_parameter_type)
-{
-  if (mu.type() != Parametric::parameter_type())
-    DUNE_PYMOR_THROW(Exception::wrong_parameter_type,
-                     "type of mu (" << mu.type() << ") does not match the parameter_type of this ("
-                     << Parametric::parameter_type() << ")!");
-  RangeType ret;
-  evaluate(x, ret, mu);
-  return ret;
-}
-
-template< class D, int d, class R, int rR, int rC >
-bool ParametricFunctionInterface< D, d, R, rR, rC >::affinely_decomposable() const
+template< class E, class D, int d, class R, int r, int rC >
+bool ParametricFunctionInterface< E, D, d, R, r, rC >::affinely_decomposable() const
 {
   if (!parametric())
     DUNE_PYMOR_THROW(Exception::this_is_not_parametric,
@@ -74,23 +45,23 @@ bool ParametricFunctionInterface< D, d, R, rR, rC >::affinely_decomposable() con
   return false;
 }
 
-template< class D, int d, class R, int rR, int rC >
-bool ParametricFunctionInterface< D, d, R, rR, rC >::has_affine_part() const
+template< class E, class D, int d, class R, int r, int rC >
+bool ParametricFunctionInterface< E, D, d, R, r, rC >::has_affine_part() const
 {
   if (!parametric())
     return true;
   if (affinely_decomposable())
     DUNE_PYMOR_THROW(Exception::you_have_to_implement_this,
-                     "since affinely_decomposable() == true, youe really do!");
+                     "Since affinely_decomposable() == true, youe really do!");
   else
     DUNE_PYMOR_THROW(Exception::requirements_not_met,
                      "Do not call has_affine_part() if affinely_decomposable() == false!");
   return false;
 }
 
-template< class D, int d, class R, int rR, int rC >
-std::shared_ptr< const typename ParametricFunctionInterface< D, d, R, rR, rC >::NonparametricType >
-ParametricFunctionInterface< D, d, R, rR, rC >::affine_part() const
+template< class E, class D, int d, class R, int r, int rC >
+std::shared_ptr< const typename ParametricFunctionInterface< E, D, d, R, r, rC >::NonparametricType >
+ParametricFunctionInterface< E, D, d, R, r, rC >::affine_part() const
 {
   if (!parametric())
     DUNE_PYMOR_THROW(Exception::this_is_not_parametric,
@@ -108,8 +79,8 @@ ParametricFunctionInterface< D, d, R, rR, rC >::affine_part() const
   return nullptr;
 }
 
-template< class D, int d, class R, int rR, int rC >
-unsigned int ParametricFunctionInterface< D, d, R, rR, rC >::num_components() const
+template< class E, class D, int d, class R, int r, int rC >
+unsigned int ParametricFunctionInterface< E, D, d, R, r, rC >::num_components() const
 {
   if (!parametric())
     return 0;
@@ -122,9 +93,9 @@ unsigned int ParametricFunctionInterface< D, d, R, rR, rC >::num_components() co
   return false;
 }
 
-template< class D, int d, class R, int rR, int rC >
-std::shared_ptr< const typename ParametricFunctionInterface< D, d, R, rR, rC >::NonparametricType >
-ParametricFunctionInterface< D, d, R, rR, rC >::component(int qq) const
+template< class E, class D, int d, class R, int r, int rC >
+std::shared_ptr< const typename ParametricFunctionInterface< E, D, d, R, r, rC >::NonparametricType >
+ParametricFunctionInterface< E, D, d, R, r, rC >::component(int qq) const
 {
   if (!parametric())
     DUNE_PYMOR_THROW(Exception::this_is_not_parametric,
@@ -142,8 +113,8 @@ ParametricFunctionInterface< D, d, R, rR, rC >::component(int qq) const
   return nullptr;
 }
 
-template< class D, int d, class R, int rR, int rC >
-std::shared_ptr< const ParameterFunctional > ParametricFunctionInterface< D, d, R, rR, rC >::coefficient(int qq) const
+template< class E, class D, int d, class R, int r, int rC >
+std::shared_ptr< const ParameterFunctional > ParametricFunctionInterface< E, D, d, R, r, rC >::coefficient(int qq) const
 {
   if (!parametric())
     DUNE_PYMOR_THROW(Exception::this_is_not_parametric,
@@ -160,181 +131,6 @@ std::shared_ptr< const ParameterFunctional > ParametricFunctionInterface< D, d, 
                      "Do not call coefficient(" << qq << ") if affinely_decomposable() == false!");
   return nullptr;
 }
-
-template class ParametricFunctionInterface< double, 1, double, 1, 2 >;
-template class ParametricFunctionInterface< double, 1, double, 1, 3 >;
-template class ParametricFunctionInterface< double, 1, double, 2, 2 >;
-template class ParametricFunctionInterface< double, 1, double, 2, 3 >;
-template class ParametricFunctionInterface< double, 1, double, 3, 2 >;
-template class ParametricFunctionInterface< double, 1, double, 3, 3 >;
-template class ParametricFunctionInterface< double, 2, double, 1, 2 >;
-template class ParametricFunctionInterface< double, 2, double, 1, 3 >;
-template class ParametricFunctionInterface< double, 2, double, 2, 2 >;
-template class ParametricFunctionInterface< double, 2, double, 2, 3 >;
-template class ParametricFunctionInterface< double, 2, double, 3, 2 >;
-template class ParametricFunctionInterface< double, 2, double, 3, 3 >;
-template class ParametricFunctionInterface< double, 3, double, 1, 2 >;
-template class ParametricFunctionInterface< double, 3, double, 1, 3 >;
-template class ParametricFunctionInterface< double, 3, double, 2, 2 >;
-template class ParametricFunctionInterface< double, 3, double, 2, 3 >;
-template class ParametricFunctionInterface< double, 3, double, 3, 2 >;
-template class ParametricFunctionInterface< double, 3, double, 3, 3 >;
-
-
-// =================================================
-// ===== ParametricFunctionInterface< ..., 1 > =====
-// =================================================
-template< class D, int d, class R, int r >
-ParametricFunctionInterface< D, d, R, r >::ParametricFunctionInterface(const ParameterType tt)
-  : Parametric(tt)
-{}
-
-template< class D, int d, class R, int r >
-ParametricFunctionInterface< D, d, R, r >::ParametricFunctionInterface(const Parametric& other)
-  : Parametric(other)
-{}
-
-template< class D, int d, class R, int r >
-ParametricFunctionInterface< D, d, R, r >::~ParametricFunctionInterface()
-{}
-
-template< class D, int d, class R, int r >
-std::string ParametricFunctionInterface< D, d, R, r >::static_id()
-{
-  return "dune.pymor.parametricfunction";
-}
-
-template< class D, int d, class R, int r >
-std::string ParametricFunctionInterface< D, d, R, r >::name() const
-{
-  return "dune.pymor.parametricfunction";
-}
-
-template< class D, int d, class R, int r >
-int ParametricFunctionInterface< D, d, R, r >::order() const
-{
-  return -1;
-}
-
-template< class D, int d, class R, int r >
-typename ParametricFunctionInterface< D, d, R, r >::RangeType
-ParametricFunctionInterface< D, d, R, r >::evaluate(const DomainType& x, const Parameter mu) const
-  throw (Exception::wrong_parameter_type)
-{
-  if (mu.type() != Parametric::parameter_type())
-    DUNE_PYMOR_THROW(Exception::wrong_parameter_type,
-                     "type of mu (" << mu.type() << ") does not match the parameter_type of this ("
-                     << Parametric::parameter_type() << ")!");
-  RangeType ret;
-  evaluate(x, ret, mu);
-  return ret;
-}
-
-template< class D, int d, class R, int r >
-bool ParametricFunctionInterface< D, d, R, r >::affinely_decomposable() const
-{
-  if (!parametric())
-    DUNE_PYMOR_THROW(Exception::this_is_not_parametric,
-                     "Do not call affinely_decomposable() if parametric() == false!");
-  return false;
-}
-
-template< class D, int d, class R, int r >
-bool ParametricFunctionInterface< D, d, R, r >::has_affine_part() const
-{
-  if (!parametric())
-    return true;
-  if (affinely_decomposable())
-    DUNE_PYMOR_THROW(Exception::you_have_to_implement_this,
-                     "since affinely_decomposable() == true, youe really do!");
-  else
-    DUNE_PYMOR_THROW(Exception::requirements_not_met,
-                     "Do not call has_affine_part() if affinely_decomposable() == false!");
-  return false;
-}
-
-template< class D, int d, class R, int r >
-std::shared_ptr< const typename ParametricFunctionInterface< D, d, R, r >::NonparametricType >
-ParametricFunctionInterface< D, d, R, r >::affine_part() const
-{
-  if (!parametric())
-    DUNE_PYMOR_THROW(Exception::this_is_not_parametric,
-                     "Do not call affine_part() if parametric() == false!");
-  if (affinely_decomposable()) {
-    if (has_affine_part())
-      DUNE_PYMOR_THROW(Exception::you_have_to_implement_this,
-                       "since affinely_decomposable() == true, youe really do!");
-    else
-      DUNE_PYMOR_THROW(Exception::requirements_not_met,
-                       "Do not call affine_part() if has_affine_part() == false!");
-  } else
-    DUNE_PYMOR_THROW(Exception::requirements_not_met,
-                     "Do not call affine_part() if affinely_decomposable() == false!");
-  return nullptr;
-}
-
-template< class D, int d, class R, int r >
-unsigned int ParametricFunctionInterface< D, d, R, r >::num_components() const
-{
-  if (!parametric())
-    return 0;
-  if (affinely_decomposable())
-    DUNE_PYMOR_THROW(Exception::you_have_to_implement_this,
-                     "since affinely_decomposable() == true, youe really do!");
-  else
-    DUNE_PYMOR_THROW(Exception::requirements_not_met,
-                     "Do not call num_components() if affinely_decomposable() == false!");
-  return false;
-}
-
-template< class D, int d, class R, int r >
-std::shared_ptr< const typename ParametricFunctionInterface< D, d, R, r >::NonparametricType >
-ParametricFunctionInterface< D, d, R, r >::component(int qq) const
-{
-  if (!parametric())
-    DUNE_PYMOR_THROW(Exception::this_is_not_parametric,
-                     "Do not call component(" << qq << ") if parametric() == false!");
-  if (affinely_decomposable()) {
-    if (num_components() > 0)
-      DUNE_PYMOR_THROW(Exception::you_have_to_implement_this,
-                       "since num_components() == " << num_components() << ", youe really do!");
-    else
-      DUNE_PYMOR_THROW(Exception::requirements_not_met,
-                       "Do not call component(" << qq << ") if num_components() == 0!");
-  } else
-    DUNE_PYMOR_THROW(Exception::requirements_not_met,
-                     "Do not call component(" << qq << ") if affinely_decomposable() == false!");
-  return nullptr;
-}
-
-template< class D, int d, class R, int r >
-std::shared_ptr< const ParameterFunctional > ParametricFunctionInterface< D, d, R, r >::coefficient(int qq) const
-{
-  if (!parametric())
-    DUNE_PYMOR_THROW(Exception::this_is_not_parametric,
-                     "Do not call coefficient(" << qq << ") if parametric() == false!");
-  if (affinely_decomposable()) {
-    if (num_components() > 0)
-      DUNE_PYMOR_THROW(Exception::you_have_to_implement_this,
-                       "since num_components() == " << num_components() << ", youe really do!");
-    else
-      DUNE_PYMOR_THROW(Exception::requirements_not_met,
-                       "Do not call coefficient(" << qq << ") if num_components() == 0!");
-  } else
-    DUNE_PYMOR_THROW(Exception::requirements_not_met,
-                     "Do not call coefficient(" << qq << ") if affinely_decomposable() == false!");
-  return nullptr;
-}
-
-template class ParametricFunctionInterface< double, 1, double, 1 >;
-template class ParametricFunctionInterface< double, 1, double, 2 >;
-template class ParametricFunctionInterface< double, 1, double, 3 >;
-template class ParametricFunctionInterface< double, 2, double, 1 >;
-template class ParametricFunctionInterface< double, 2, double, 2 >;
-template class ParametricFunctionInterface< double, 2, double, 3 >;
-template class ParametricFunctionInterface< double, 3, double, 1 >;
-template class ParametricFunctionInterface< double, 3, double, 2 >;
-template class ParametricFunctionInterface< double, 3, double, 3 >;
 
 
 } // namespace Pymor
--- dune/pymor/functions/interfaces.hh
+++ dune/pymor/functions/interfaces.hh
@@ -23,24 +23,27 @@ namespace Pymor {
 
 /**
  * \brief Interface for matrix valued (possibly parametric) functions.
- * \note  See specialization (rangeDimCols = 1) for scalar and vector valued functions.
  */
-template< class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDimRows, int rangeDimCols = 1 >
+template< class EntityImp, class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDim, int rangeDimCols = 1 >
 class ParametricFunctionInterface
   : public Parametric
 {
 public:
+  typedef EntityImp EntityType;
+
   typedef DomainFieldImp                                  DomainFieldType;
   static const unsigned int                               dimDomain = domainDim;
-  typedef Dune::FieldVector< DomainFieldType, dimDomain > DomainType;
 
   typedef RangeFieldImp                                                   RangeFieldType;
-  static const unsigned int                                               dimRangeRows = rangeDimRows;
+  static const unsigned int                                               dimRange = rangeDim;
   static const unsigned int                                               dimRangeCols = rangeDimCols;
-  typedef Dune::FieldMatrix< RangeFieldType, dimRangeRows, dimRangeCols > RangeType;
 
-  typedef Stuff::FunctionInterface< DomainFieldType, dimDomain,
-                                    RangeFieldType, dimRangeRows, dimRangeCols > NonparametricType;
+  typedef Stuff::LocalizableFunctionInterface
+      < EntityType, DomainFieldType, dimDomain, RangeFieldType, dimRange, dimRangeCols > NonparametricType;
+
+  typedef typename NonparametricType::DomainType        DomainType;
+  typedef typename NonparametricType::RangeType         RangeType;
+  typedef typename NonparametricType::JacobianRangeType JacobianRangeType;
 
   ParametricFunctionInterface(const ParameterType tt = ParameterType());
 
@@ -51,13 +54,6 @@ public:
   static std::string static_id();
 
   virtual std::string name() const;
-
-  virtual int order() const;
-
-  virtual void evaluate(const DomainType& x, RangeType& ret, const Parameter mu = Parameter()) const = 0;
-
-  virtual RangeType evaluate(const DomainType& x, const Parameter mu = Parameter()) const
-    throw (Exception::wrong_parameter_type);
 
   virtual bool affinely_decomposable() const;
 
@@ -73,58 +69,9 @@ public:
 }; // class ParametricFunctionInterface
 
 
-/**
- * \brief Interface for scalar and vector valued (possibly parametric) functions.
- */
-template< class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDimRows >
-class ParametricFunctionInterface< DomainFieldImp, domainDim, RangeFieldImp, rangeDimRows, 1 >
-  : public Parametric
-{
-public:
-  typedef DomainFieldImp                                  DomainFieldType;
-  static const unsigned int                               dimDomain = domainDim;
-  typedef Dune::FieldVector< DomainFieldType, dimDomain > DomainType;
-
-  typedef RangeFieldImp                                 RangeFieldType;
-  static const unsigned int                             dimRangeRows = rangeDimRows;
-  static const unsigned int                             dimRangeCols = 1;
-  static const unsigned int                             dimRange = dimRangeRows;
-  typedef Dune::FieldVector< RangeFieldType, dimRange > RangeType;
-
-  typedef Stuff::FunctionInterface< DomainFieldType, dimDomain, RangeFieldType, dimRange > NonparametricType;
-
-  ParametricFunctionInterface(const ParameterType tt = ParameterType());
-
-  ParametricFunctionInterface(const Parametric& other);
-
-  virtual ~ParametricFunctionInterface();
-
-  static std::string static_id();
-
-  virtual std::string name() const;
-
-  virtual int order() const;
-
-  virtual void evaluate(const DomainType& x, RangeType& ret, const Parameter mu = Parameter()) const = 0;
-
-  virtual RangeType evaluate(const DomainType& x, const Parameter mu = Parameter()) const
-    throw (Exception::wrong_parameter_type);
-
-  virtual bool affinely_decomposable() const;
-
-  virtual bool has_affine_part() const;
-
-  virtual std::shared_ptr< const NonparametricType > affine_part() const;
-
-  virtual unsigned int num_components() const;
-
-  virtual std::shared_ptr< const NonparametricType > component(int ii) const;
-
-  virtual std::shared_ptr< const ParameterFunctional > coefficient(int ii) const;
-}; // class ParametricFunctionInterface< ..., 1 >
-
-
 } // namespace Pymor
 } // namespace Dune
+
+#include "interfaces.cc"
 
 #endif // DUNE_PYMOR_FUNCTIONS_INTERFACES_HH
