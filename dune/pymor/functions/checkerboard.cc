212a08aab201f3bb2ce888bed07a79bce445b8d9
Felix Albrecht <mail@felixalbrecht.de>
2013-10-23 00:38:14 +0200

    [functions.checkerboard] major update * moved definitions to header and in class * use DUNE_PYMOR_SSIZE_T, refs #8

--- dune/pymor/functions/checkerboard.cc
+++ dune/pymor/functions/checkerboard.cc
@@ -1,110 +1,54 @@
+// This file is part of the dune-pymor project:
+//   https://github.com/pyMor/dune-pymor
+// Copyright Holders: Felix Albrecht, Stephan Rave
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#include "config.h"
+
 #include "checkerboard.hh"
 
-namespace Dune {
-namespace Pymor {
-namespace Function {
+#define DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_DIMRANGE(etype, ddim) \
+  DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_DIMRANGECOLS(etype, ddim, 1) \
+  DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_DIMRANGECOLS(etype, ddim, 2) \
+  DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_DIMRANGECOLS(etype, ddim, 3)
 
+#define DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_DIMRANGECOLS(etype, ddim, rdim) \
+  DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_DOMAINFIELDTYPES(etype, ddim, rdim, 1) \
+  DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_DOMAINFIELDTYPES(etype, ddim, rdim, 2) \
+  DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_DOMAINFIELDTYPES(etype, ddim, rdim, 3)
 
-template< class D, int d, class R >
-std::string Checkerboard< D, d, R >::static_id()
-{
-  return ParametricFunctionInterface< DomainFieldType, dimDomain,
-                                      RangeFieldType, 1, 1 >::static_id() + ".checkerboard";
-}
+#define DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_DOMAINFIELDTYPES(etype, ddim, rdim, rcdim) \
+  DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_RANGEFIELDTYPES(etype, double, ddim, rdim, rcdim)
 
-template< class D, int d, class R >
-Checkerboard< D, d, R >::Checkerboard(const DomainType& lowLeft,
-                                      const DomainType& upRight,
-                                      const std::vector< size_t >& numElements,
-                                      const std::string parameterName,
-                                      const std::string name)
-  : BaseType(name, 0)
-{
-  // check input
-  if (numElements.size() < dimDomain)
-    DUNE_PYMOR_THROW(Exception::sizes_do_not_match,
-                     "numElements has to be at least of size " << dimDomain << " is(" << numElements.size()
-                     << ")!");
-  size_t parameterSize = 1;
-  for (size_t dd = 0; dd < dimDomain; ++dd) {
-    if (lowLeft[dd] >= upRight[dd])
-      DUNE_PYMOR_THROW(Exception::this_does_not_make_any_sense,
-                       "lowLeft[" << dd << "] = " << lowLeft[dd] << " has to be smaller than upRight["
-                       << dd << "]!");
-    if (numElements[dd] <= 0)
-      DUNE_PYMOR_THROW(Exception::index_out_of_range,
-                       "numElements[" << dd << "] has to be positive (is " << numElements[dd] << ")!");
-    parameterSize *= numElements[dd];
-  }
-  if (parameterName.empty())
-    DUNE_PYMOR_THROW(Exception::wrong_input, "parameterName must not be empty!");
-  // build parameter
-  const ParameterType parameterType(parameterName, parameterSize);
-  // create the coefficients and components
-  typedef Dune::Stuff::FunctionCheckerboard< DomainFieldType, dimDomain, RangeFieldType, 1 > NonparametricType;
-  for (size_t ii = 0; ii < parameterSize; ++ii) {
-    std::vector< RangeType > indicator(parameterSize, RangeType(0));
-    indicator[ii] = RangeType(1);
-    BaseType::register_component(new NonparametricType(lowLeft, upRight, numElements, indicator),
-                                 new ParameterFunctional(parameterType,
-                                                         parameterName + "[" + DSC::toString(ii) + "]"));
-  } // create the coefficients and components
-} // Checkerboard()
+#define DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_RANGEFIELDTYPES(etype, dftype, ddim, rdim, rcdim) \
+  DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LAST_EXPANSION(etype, dftype, ddim, double, rdim, rcdim) \
+  DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LAST_EXPANSION(etype, dftype, ddim, long double, rdim, rcdim)
 
-template< class D, int d, class R >
-Dune::ParameterTree Checkerboard< D, d, R >::defaultSettings(const std::string subName)
-{
-  Dune::ParameterTree description;
-  description["lowerLeft"] = "[0.0; 0.0; 0.0]";
-  description["upperRight"] = "[1.0; 1.0; 1.0]";
-  description["numElements"] = "[2; 2; 2]";
-  description["parameterName"] = "value";
-  description["name"] = static_id();
-  if (subName.empty())
-    return description;
-  else {
-    DSC::ExtendedParameterTree extendedDescription;
-    extendedDescription.add(description, subName);
-    return extendedDescription;
-  }
-} // ... defaultSettings(...)
+#define DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LAST_EXPANSION(etype, dftype, ddim, rftype, rdim, rcdim) \
+  template class Dune::Pymor::Function::Checkerboard< etype, dftype, ddim, rftype, rdim, rcdim >;
 
-template< class D, int d, class R >
-typename Checkerboard< D, d, R >::ThisType* Checkerboard< D, d, R >::create(const DSC::ExtendedParameterTree settings)
-throw (Exception::this_does_not_make_any_sense)
-{
-  // get data
-  const std::string nm = settings.get< std::string >("name", static_id());
-  const std::string paramName = settings.get< std::string >("parameterName", "value");
-  const std::vector< DomainFieldType > lowerLefts = settings.getVector("lowerLeft", DomainFieldType(0), dimDomain);
-  const std::vector< DomainFieldType > upperRights = settings.getVector("upperRight",
-                                                                           DomainFieldType(1),
-                                                                           dimDomain);
-  const std::vector< size_t > numElements = settings.getVector("numElements", size_t(1), dimDomain);
-  // get paramSize
-  size_t paramSize = 1u;
-  for (size_t dd = 0; dd < dimDomain; ++dd) {
-    if (numElements[dd] <= 0)
-      DUNE_PYMOR_THROW(Exception::this_does_not_make_any_sense,
-                       "numElements[" << dd << "] has to be posititve (is " << numElements[dd] << ")!");
-    paramSize *= numElements[dd];
-  }
-  // convert and leave the checks to the constructor
-  DomainType lowerLeft;
-  DomainType upperRight;
-  for (size_t dd = 0; dd < dimDomain; ++dd) {
-    lowerLeft[dd] = lowerLefts[dd];
-    upperRight[dd] = upperRights[dd];
-  }
-  // create and return
-  return new ThisType(lowerLeft, upperRight, numElements, paramName, nm);
-} // ... create(...)
+#ifdef HAVE_DUNE_GRID
 
-template class Checkerboard< double, 1, double >;
-template class Checkerboard< double, 2, double >;
-template class Checkerboard< double, 3, double >;
+DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_DIMRANGE(DunePymorFunctionsInterfacesSGrid1dEntityType, 1)
+DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_DIMRANGE(DunePymorFunctionsInterfacesSGrid2dEntityType, 2)
+DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_DIMRANGE(DunePymorFunctionsInterfacesSGrid3dEntityType, 3)
 
+DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_DIMRANGE(DunePymorFunctionsInterfacesYaspGrid1dEntityType, 1)
+DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_DIMRANGE(DunePymorFunctionsInterfacesYaspGrid2dEntityType, 2)
+DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_DIMRANGE(DunePymorFunctionsInterfacesYaspGrid3dEntityType, 3)
 
-} // namespace Function
-} // namespace Pymor
-} // namespace Dune
+# if HAVE_ALUGRID_SERIAL_H || HAVE_ALUGRID_PARALLEL_H
+
+DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_DIMRANGE(DunePymorFunctionsInterfacesAluSimplexGrid2dEntityType, 2)
+DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_DIMRANGE(DunePymorFunctionsInterfacesAluSimplexGrid3dEntityType, 3)
+DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_DIMRANGE(DunePymorFunctionsInterfacesAluCubeGrid3dEntityType, 3)
+
+# endif // HAVE_ALUGRID_SERIAL_H || HAVE_ALUGRID_PARALLEL_H
+#endif // HAVE_DUNE_GRID
+
+#undef DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LAST_EXPANSION
+#undef DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_RANGEFIELDTYPES
+#undef DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_DOMAINFIELDTYPES
+#undef DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_DIMRANGECOLS
+#undef DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_DIMRANGE
+#undef DUNE_PYMOR_FUNCTION_CHECKERBOARD_CC_LIST_CLASSES
--- dune/pymor/functions/checkerboard.hh
+++ dune/pymor/functions/checkerboard.hh
@@ -22,37 +22,163 @@ namespace Pymor {
 namespace Function {
 
 
-template< class DomainFieldImp, int domainDim, class RangeFieldImp >
+template< class EntityImp, class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDim, int rangeDimCols = 1 >
 class Checkerboard
-  : public Function::AffinelyDecomposableDefault< DomainFieldImp, domainDim, RangeFieldImp, 1, 1 >
+  : public Function::AffinelyDecomposableDefault
+        < EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, rangeDimCols >
 {
-  typedef Function::AffinelyDecomposableDefault< DomainFieldImp, domainDim, RangeFieldImp, 1, 1 > BaseType;
+  typedef Checkerboard< EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, rangeDimCols > ThisType;
+  typedef Function::AffinelyDecomposableDefault
+      < EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, rangeDimCols > BaseType;
 public:
-  typedef Checkerboard< DomainFieldImp, domainDim, RangeFieldImp > ThisType;
+  typedef typename BaseType::NonparametricType  NonparametricType;
+  typedef typename BaseType::EntityType         EntityType;
 
   typedef typename BaseType::DomainFieldType  DomainFieldType;
   static const unsigned int                   dimDomain = BaseType::dimDomain;
   typedef typename BaseType::DomainType       DomainType;
-  typedef typename BaseType::RangeFieldType   RangeFieldType;
-  typedef typename BaseType::RangeType        RangeType;
 
-  static std::string static_id();
+  typedef typename BaseType::RangeFieldType RangeFieldType;
+  static const unsigned int                 dimRange = BaseType::dimRange;
+  static const unsigned int                 dimRangeCols = BaseType::dimRangeCols;
+  typedef typename BaseType::RangeType      RangeType;
 
-  Checkerboard(const DomainType& lowLeft,
-               const DomainType& upRight,
+  static std::string static_id()
+  {
+    return BaseType::BaseType::static_id() + ".checkerboard";
+  }
+
+  Checkerboard(const std::vector< DomainFieldType >& lowerLeft,
+               const std::vector< DomainFieldType >& upperRight,
                const std::vector< size_t >& numElements,
                const std::string parameterName = "value",
-               const std::string name = static_id());
+               const std::string name = static_id())
+    : BaseType(name)
+  {
+    // check input
+    if (numElements.size() < dimDomain)
+      DUNE_PYMOR_THROW(Exception::sizes_do_not_match,
+                       "numElements has to be at least of size " << dimDomain << " is(" << numElements.size()
+                       << ")!");
+    size_t parameterSize = 1;
+    for (size_t dd = 0; dd < dimDomain; ++dd) {
+      if (lowerLeft[dd] >= upperRight[dd])
+        DUNE_PYMOR_THROW(Exception::this_does_not_make_any_sense,
+                         "lowerLeft[" << dd << "] = " << lowerLeft[dd] << " has to be smaller than upperRight["
+                         << dd << "]!");
+      if (numElements[dd] <= 0)
+        DUNE_PYMOR_THROW(Exception::index_out_of_range,
+                         "numElements[" << dd << "] has to be positive (is " << numElements[dd] << ")!");
+      parameterSize *= numElements[dd];
+    }
+    if (parameterName.empty())
+      DUNE_PYMOR_THROW(Exception::wrong_input, "parameterName must not be empty!");
+    // build parameter
+    const ParameterType parameterType(parameterName, parameterSize);
+    // create the coefficients and components
+    typedef Dune::Stuff::Function::Checkerboard
+        < EntityType, DomainFieldType, dimDomain, RangeFieldType, dimRange, dimRangeCols >
+      NonparametricCheckerboardType;
+    for (size_t ii = 0; ii < parameterSize; ++ii) {
+      std::vector< RangeType > indicator(parameterSize, RangeType(0));
+      indicator[ii] = RangeType(1);
+      BaseType::register_component(new NonparametricCheckerboardType(std::vector< DomainFieldType >(lowerLeft),
+                                                                     std::vector< DomainFieldType >(upperRight),
+                                                                     std::vector< size_t >(numElements),
+                                                                     std::move(indicator)),
+                                   new ParameterFunctional(parameterType,
+                                                           parameterName + "[" + DSC::toString(ii) + "]"));
+    } // create the coefficients and components
+  } // Checkerboard()
 
-  static Dune::ParameterTree defaultSettings(const std::string subName = "");
+  static Dune::ParameterTree defaultSettings(const std::string subName = "")
+  {
+    Dune::ParameterTree description;
+    description["lowerLeft"] = "[0.0; 0.0; 0.0]";
+    description["upperRight"] = "[1.0; 1.0; 1.0]";
+    description["numElements"] = "[2; 2; 2]";
+    description["parameterName"] = "value";
+    description["name"] = static_id();
+    if (subName.empty())
+      return description;
+    else {
+      DSC::ExtendedParameterTree extendedDescription;
+      extendedDescription.add(description, subName);
+      return extendedDescription;
+    }
+  } // ... defaultSettings(...)
 
   static ThisType* create(const DSC::ExtendedParameterTree settings = defaultSettings())
-  throw (Exception::this_does_not_make_any_sense);
+
+  {
+    // get data
+    const std::string nm = settings.get< std::string >("name", static_id());
+    const std::string paramName = settings.get< std::string >("parameterName", "value");
+    std::vector< DomainFieldType > lowerLeft = settings.getVector("lowerLeft", DomainFieldType(0), dimDomain);
+    std::vector< DomainFieldType > upperRight = settings.getVector("upperRight", DomainFieldType(1), dimDomain);
+    std::vector< size_t > numElements = settings.getVector("numElements", size_t(1), dimDomain);
+    // get paramSize
+    size_t paramSize = 1;
+    for (size_t dd = 0; dd < dimDomain; ++dd) {
+      if (numElements[dd] <= 0)
+        DUNE_PYMOR_THROW(Exception::this_does_not_make_any_sense,
+                         "numElements[" << dd << "] has to be posititve (is " << numElements[dd] << ")!");
+      paramSize *= numElements[dd];
+    }
+    // create and return
+    return new ThisType(lowerLeft, upperRight, numElements, paramName, nm);
+  } // ... create(...)
 }; // class Checkerboard
 
 
 } // namespace Function
 } // namespace Pymor
 } // namespace Dune
+
+#define DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_DIMRANGE(etype, ddim) \
+  DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_DIMRANGECOLS(etype, ddim, 1) \
+  DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_DIMRANGECOLS(etype, ddim, 2) \
+  DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_DIMRANGECOLS(etype, ddim, 3)
+
+#define DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_DIMRANGECOLS(etype, ddim, rdim) \
+  DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_DOMAINFIELDTYPES(etype, ddim, rdim, 1) \
+  DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_DOMAINFIELDTYPES(etype, ddim, rdim, 2) \
+  DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_DOMAINFIELDTYPES(etype, ddim, rdim, 3)
+
+#define DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_DOMAINFIELDTYPES(etype, ddim, rdim, rcdim) \
+  DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_RANGEFIELDTYPES(etype, double, ddim, rdim, rcdim)
+
+#define DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_RANGEFIELDTYPES(etype, dftype, ddim, rdim, rcdim) \
+  DUNE_PYMOR_FUNCTION_CHECKERBOARD_LAST_EXPANSION(etype, dftype, ddim, double, rdim, rcdim) \
+  DUNE_PYMOR_FUNCTION_CHECKERBOARD_LAST_EXPANSION(etype, dftype, ddim, long double, rdim, rcdim)
+
+#define DUNE_PYMOR_FUNCTION_CHECKERBOARD_LAST_EXPANSION(etype, dftype, ddim, rftype, rdim, rcdim) \
+  extern template class Dune::Pymor::Function::Checkerboard< etype, dftype, ddim, rftype, rdim, rcdim >;
+
+#ifdef HAVE_DUNE_GRID
+
+DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_DIMRANGE(DunePymorFunctionsInterfacesSGrid1dEntityType, 1)
+DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_DIMRANGE(DunePymorFunctionsInterfacesSGrid2dEntityType, 2)
+DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_DIMRANGE(DunePymorFunctionsInterfacesSGrid3dEntityType, 3)
+
+DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_DIMRANGE(DunePymorFunctionsInterfacesYaspGrid1dEntityType, 1)
+DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_DIMRANGE(DunePymorFunctionsInterfacesYaspGrid2dEntityType, 2)
+DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_DIMRANGE(DunePymorFunctionsInterfacesYaspGrid3dEntityType, 3)
+
+# if HAVE_ALUGRID_SERIAL_H || HAVE_ALUGRID_PARALLEL_H
+
+DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_DIMRANGE(DunePymorFunctionsInterfacesAluSimplexGrid2dEntityType, 2)
+DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_DIMRANGE(DunePymorFunctionsInterfacesAluSimplexGrid3dEntityType, 3)
+DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_DIMRANGE(DunePymorFunctionsInterfacesAluCubeGrid3dEntityType, 3)
+
+# endif // HAVE_ALUGRID_SERIAL_H || HAVE_ALUGRID_PARALLEL_H
+#endif // HAVE_DUNE_GRID
+
+#undef DUNE_PYMOR_FUNCTION_CHECKERBOARD_LAST_EXPANSION
+#undef DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_RANGEFIELDTYPES
+#undef DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_DOMAINFIELDTYPES
+#undef DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_DIMRANGECOLS
+#undef DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_DIMRANGE
+#undef DUNE_PYMOR_FUNCTION_CHECKERBOARD_LIST_CLASSES
 
 #endif // DUNE_PYMOR_FUNCTIONS_CHECKERBOARD_HH
