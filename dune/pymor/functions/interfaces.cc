6ba76eaec07256739b7bad9788441b1f4858878a
Felix Albrecht <mail@felixalbrecht.de>
2013-11-19 18:18:15 +0100

    replaced all occurences of pyMor with pymor

--- dune/pymor/functions/interfaces.cc
+++ dune/pymor/functions/interfaces.cc
@@ -1,5 +1,5 @@
 ﻿// This file is part of the dune-pymor project:
-//   https://github.com/pyMor/dune-pymor
+//   https://github.com/pymor/dune-pymor
 // Copyright Holders: Felix Albrecht, Stephan Rave
 // License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
