57adb8240cf0015e1e10f2e9fd4f090a8d896a27
Felix Albrecht <mail@felixalbrecht.de>
2013-12-13 17:57:14 +0100

    Revert "[examples...bindings_generator] began to update"
    This reverts commit 81c1b22742bb504e0d6b34251e8fd01098557575.

--- examples/stationarylinear_bindings_generator.py
+++ examples/stationarylinear_bindings_generator.py
@@ -8,7 +8,7 @@ import sys
 from pybindgen import param, retval
 
 from dune.pymor.core import prepare_python_bindings, inject_lib_dune_pymor, finalize_python_bindings
-#from dune.pymor.discretizations import inject_StationaryDiscretizationImplementation
+from dune.pymor.discretizations import inject_StationaryDiscretizationImplementation
 
 
 def inject_Example(module):
@@ -24,17 +24,17 @@ if __name__ == '__main__':
     module, pybindgen_filename = prepare_python_bindings(sys.argv[1:])
     # add all of libdunepymor
     module, exceptions, interfaces, CONFIG_H = inject_lib_dune_pymor(module)
-#    # add example user code
-#    inject_Example(module)
-#    # add the users discretization
-#    discretization = inject_StationaryDiscretizationImplementation(
-#        module, exceptions, interfaces, CONFIG_H,
-#        'Example::SimpleDiscretization',
-#        Traits={'VectorType': 'Dune::Pymor::LA::DuneDynamicVector< double >',
-#                'OperatorType': 'Dune::Pymor::Operators::LinearAffinelyDecomposedContainerBased< Dune::Pymor::Operators::DuneDynamic< double > >',
-#                'FunctionalType': 'Dune::Pymor::Functionals::LinearAffinelyDecomposedVectorBased< Dune::Pymor::LA::DuneDynamicVector< double > >',
-#                'ProductType': 'Dune::Pymor::Operators::LinearAffinelyDecomposedContainerBased< Dune::Pymor::Operators::DuneDynamic< double > >'})
-#    # and add the custom constructor to the discretization
-#    discretization.add_constructor([param('const Example::AnalyticalProblem *', 'prob', transfer_ownership=True)])
+    # add example user code
+    inject_Example(module)
+    # add the users discretization
+    discretization = inject_StationaryDiscretizationImplementation(
+        module, exceptions, interfaces, CONFIG_H,
+        'Example::SimpleDiscretization',
+        Traits={'VectorType': 'Dune::Pymor::LA::DuneDynamicVector< double >',
+                'OperatorType': 'Dune::Pymor::Operators::LinearAffinelyDecomposedContainerBased< Dune::Pymor::Operators::DuneDynamic< double > >',
+                'FunctionalType': 'Dune::Pymor::Functionals::LinearAffinelyDecomposedVectorBased< Dune::Pymor::LA::DuneDynamicVector< double > >',
+                'ProductType': 'Dune::Pymor::Operators::LinearAffinelyDecomposedContainerBased< Dune::Pymor::Operators::DuneDynamic< double > >'})
+    # and add the custom constructor to the discretization
+    discretization.add_constructor([param('const Example::AnalyticalProblem *', 'prob', transfer_ownership=True)])
     # and finally write the pybindgen .cc file
     finalize_python_bindings(module, pybindgen_filename)
