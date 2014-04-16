0b2264c44197d10addf85b43798f286a7beb1f05
Felix Schindler <felix@schindlerfamily.de>
2014-04-16 16:07:21 +0200

    removed config.h includes from headers

--- dune/pymor/operators/eigen.hh
+++ dune/pymor/operators/eigen.hh
@@ -3,8 +3,6 @@
 // Copyright Holders: Stephan Rave, Felix Schindler
 // License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
-#include "config.h"
-
 #ifndef DUNE_PYMOR_OPERATORS_EIGEN_HH
 #define DUNE_PYMOR_OPERATORS_EIGEN_HH
 
