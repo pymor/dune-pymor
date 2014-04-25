ba1e5f921720fd27ac08533ff697e8bd36924527
Felix Schindler <felix@schindlerfamily.de>
2014-04-25 11:12:02 +0200

    [license] minor update

--- dune/pymor/operators/common.hh
+++ dune/pymor/operators/common.hh
@@ -1,6 +1,6 @@
 // This file is part of the dune-pymor project:
 //   https://github.com/pymor/dune-pymor
-// Copyright Holders: Stephan Rave, Felix Schindler
+// Copyright holders: Stephan Rave, Felix Schindler
 // License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
 #ifndef DUNE_PYMOR_OPERATORS_COMMONDENSE_HH
