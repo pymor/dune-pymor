10be10d4667ffdc5fd3334b0b51bf783ef318255
Felix Schindler <felix.schindler@wwu.de>
2014-08-15 15:33:13 +0200

    [common.exceptions] marked DUNE_PYMOR_THROW, PymorException as deprecated

--- dune/pymor/common/exceptions.hh
+++ dune/pymor/common/exceptions.hh
@@ -6,9 +6,7 @@
 #ifndef DUNE_PYMOR_COMMON_EXCEPTIONS_HH
 #define DUNE_PYMOR_COMMON_EXCEPTIONS_HH
 
-#include <dune/common/exceptions.hh>
-
-#include <dune/stuff/common/color.hh>
+#include <dune/stuff/common/exceptions.hh>
 
 namespace Dune {
 namespace Pymor {
@@ -46,7 +44,7 @@ if (a.size() != b.size)
  */
 #define DUNE_PYMOR_THROW(E, m) do {\
   E th__ex; \
-  std::ostringstream th__msg; \
+  std::ostringstream __attribute__((deprecated)) /* use DUNE_THROW instead (will be removed on 03.09.2014)! */ th__msg; \
   th__msg << m; \
   std::ostringstream th__out; \
   th__out << Dune::Stuff::Common::Colors::red << # E << "\033[0m\n"; \
@@ -58,6 +56,7 @@ if (a.size() != b.size)
   th__ex.pymorMessage(th__out.str()); \
   throw th__ex; \
 } while (0)
+
 
 namespace Dune {
 
@@ -72,7 +71,9 @@ class foo_is_not_bar : public Dune::PymorException {};
  *        Instead of throwing a Dune::PymorException directly, it is convenient to use the macro DUNE_PYMOR_THROW().
  * \see   DUNE_PYMOR_THROW, Dune::Exception
  */
-class PymorException
+class
+  DUNE_DEPRECATED_MSG("Use one of the exceptions in Stuff::Exceptions:: or Pymor::Exceptions instead (will be removed on 03.09.2014)!")
+      PymorException
   : public Dune::Exception
 {
 public:
@@ -98,7 +99,9 @@ namespace Exception {
  *        This can be used when dynamically deriving from an interface, see Dune::Pymor::La::VectorInterface for an
  *        example.
  */
-class types_are_not_compatible
+class
+  DUNE_DEPRECATED_MSG("Use one of the exceptions in Stuff::Exceptions:: or Pymor::Exceptions instead (will be removed on 03.09.2014)!")
+     types_are_not_compatible
   : public Dune::NotImplemented
   , public Dune::PymorException
 {};
@@ -106,7 +109,9 @@ class types_are_not_compatible
 /**
  * \brief Exception for the case where two arguments do not match in size, i.e. two vectors do not have the same lenght.
  */
-class sizes_do_not_match
+class
+  DUNE_DEPRECATED_MSG("Use one of the exceptions in Stuff::Exceptions:: or Pymor::Exceptions instead (will be removed on 03.09.2014)!")
+     sizes_do_not_match
   : public Dune::RangeError
   , public Dune::PymorException
 {};
@@ -114,7 +119,9 @@ class sizes_do_not_match
 /**
  * \brief Exception for indices which are out of the admissible range.
  */
-class index_out_of_range
+class
+  DUNE_DEPRECATED_MSG("Use one of the exceptions in Stuff::Exceptions:: or Pymor::Exceptions instead (will be removed on 03.09.2014)!")
+     index_out_of_range
   : public Dune::RangeError
   , public Dune::PymorException
 {};
@@ -122,7 +129,9 @@ class index_out_of_range
 /**
  * \brief Exception for invalied keys in a dictionary/map context, i.e. an empty string "".
  */
-class key_is_not_valid
+class
+  DUNE_DEPRECATED_MSG("Use one of the exceptions in Stuff::Exceptions:: or Pymor::Exceptions instead (will be removed on 03.09.2014)!")
+      key_is_not_valid
   : public Dune::RangeError
   , public Dune::PymorException
 {};
@@ -130,7 +139,9 @@ class key_is_not_valid
 /**
  * \brief Exception for methods the user has to implement.
  */
-class you_have_to_implement_this
+class
+  DUNE_DEPRECATED_MSG("Use one of the exceptions in Stuff::Exceptions:: or Pymor::Exceptions instead (will be removed on 03.09.2014)!")
+     you_have_to_implement_this
   : public Dune::NotImplemented
   , public Dune::PymorException
 {};
@@ -138,7 +149,9 @@ class you_have_to_implement_this
 /**
  * \brief Exception for a wrong Dune::Pymor::ParameterType.
  */
-class wrong_parameter_type
+class
+  DUNE_DEPRECATED_MSG("Use one of the exceptions in Stuff::Exceptions:: or Pymor::Exceptions instead (will be removed on 03.09.2014)!")
+     wrong_parameter_type
   : public Dune::RangeError
   , public Dune::PymorException
 {};
@@ -146,7 +159,9 @@ class wrong_parameter_type
 /**
  * \brief Exception for nonparametric objects that are treated as if they were parametric.
  */
-class this_is_not_parametric
+class
+  DUNE_DEPRECATED_MSG("Use one of the exceptions in Stuff::Exceptions:: or Pymor::Exceptions instead (will be removed on 03.09.2014)!")
+      this_is_not_parametric
   : public Dune::InvalidStateException
   , public Dune::PymorException
 {};
@@ -154,7 +169,9 @@ class this_is_not_parametric
 /**
  * \brief Exception for unlogical situations.
  */
-class this_does_not_make_any_sense
+class
+  DUNE_DEPRECATED_MSG("Use one of the exceptions in Stuff::Exceptions:: or Pymor::Exceptions instead (will be removed on 03.09.2014)!")
+     this_does_not_make_any_sense
   : public Dune::InvalidStateException
   , public Dune::PymorException
 {};
@@ -163,7 +180,9 @@ class this_does_not_make_any_sense
  * \brief Exception for calling an object in an unsuitable state, i.e. if an Dune::Pymor::AffinelyDecomposedOperato
  *        reports hasAffinePart() == false and the user calls affinePart();
  */
-class requirements_not_met
+class
+  DUNE_DEPRECATED_MSG("Use one of the exceptions in Stuff::Exceptions:: or Pymor::Exceptions instead (will be removed on 03.09.2014)!")
+      requirements_not_met
   : public Dune::InvalidStateException
   , public Dune::PymorException
 {};
@@ -171,7 +190,9 @@ class requirements_not_met
 /**
  * \brief Exception for wrong input arguments.
  */
-class wrong_input
+class
+  DUNE_DEPRECATED_MSG("Use one of the exceptions in Stuff::Exceptions:: or Pymor::Exceptions instead (will be removed on 03.09.2014)!")
+      wrong_input
   : public Dune::RangeError
   , public Dune::PymorException
 {};
@@ -179,7 +200,9 @@ class wrong_input
 /**
  * \brief Exception for operators/matrices which are not invertible.
  */
-class not_invertible
+class
+  DUNE_DEPRECATED_MSG("Use one of the exceptions in Stuff::Exceptions:: or Pymor::Exceptions instead (will be removed on 03.09.2014)!")
+     not_invertible
   : public Dune::MathError
   , public Dune::PymorException
 {};
@@ -187,7 +210,9 @@ class not_invertible
 /**
  * \brief Exception for linear solvers which did not succeed.
  */
-class linear_solver_failed
+class
+  DUNE_DEPRECATED_MSG("Use one of the exceptions in Stuff::Exceptions:: or Pymor::Exceptions instead (will be removed on 03.09.2014)!")
+     linear_solver_failed
   : public Dune::MathError
   , public Dune::PymorException
 {};
@@ -195,7 +220,9 @@ class linear_solver_failed
 /**
  * \brief Exception for input/output errors.
  */
-class io_error
+class
+  DUNE_DEPRECATED_MSG("Use one of the exceptions in Stuff::Exceptions:: or Pymor::Exceptions instead (will be removed on 03.09.2014)!")
+     io_error
   : public Dune::IOError
   , public Dune::PymorException
 {};
@@ -203,7 +230,9 @@ class io_error
 /**
  * \brief Exception for wrong options.
  */
-class wrong_option_given
+class
+  DUNE_DEPRECATED_MSG("Use one of the exceptions in Stuff::Exceptions:: or Pymor::Exceptions instead (will be removed on 03.09.2014)!")
+      wrong_option_given
   : public Dune::RangeError
   , public Dune::PymorException
 {};
