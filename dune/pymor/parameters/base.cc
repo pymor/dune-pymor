f2b61a7a226d7aa97b19dbe6b01050c759e1b35b
Felix Schindler <felix.schindler@wwu.de>
2014-09-02 11:50:54 +0200

    [parameters.base] Let map_parameter() work in the nonparametric case.

--- dune/pymor/parameters/base.cc
+++ dune/pymor/parameters/base.cc
@@ -487,30 +487,10 @@ void Parametric::inherit_parameter_type(const Parametric& other, const std::stri
 
 Parameter Parametric::map_parameter(const Parameter& mu, const std::string id) const
 {
-  if (inherits_map_.empty())
-    DUNE_THROW(Stuff::Exceptions::wrong_input_given, "there is nothing to map!");
   const auto result = inherits_map_.find(id);
-  if (result == inherits_map_.end()) {
+  if (result == inherits_map_.end())
     return Parameter();
-//    std::stringstream msg;
-//    msg << "id has to be one of {";
-//    size_t count = 0;
-//    const auto itEnd = inherits_map_.end();
-//    auto it = inherits_map_.begin();
-//    if (inherits_map_.size() > 1) {
-//      for (; it != itEnd; ++it) {
-//        if (count < inherits_map_.size() - 1) {
-//          msg << "'" << it->first << "', ";
-//          ++count;
-//        } else {
-//          ++it;
-//          break;
-//        }
-//      }
-//    }
-//    msg << "'" << it->first << "'} (is '" << id << "')!";
-//    DUNE_THROW(Stuff::Exceptions::wrong_input_given, msg.str());
-  } else {
+  else {
     const ParameterType& localType = result->second;
     Parameter muLocal;
     for (auto key : localType.keys())
